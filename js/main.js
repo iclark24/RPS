606f679f56fddbf71638eccf5467f21929636f36
ian <clarksware24@gmail.com>
2018-11-30 17:30:06 -0700

    reduce js code

--- js/main.js
+++ js/main.js
@@ -1,55 +1,25 @@
 var result = ["You Win!", "You Lose!", "It's a Draw!"];
 var choices = ["Rock", "Paper", "Scissors"]
 
-document.getElementById("option1").addEventListener("click", playrock)
-document.getElementById("option2").addEventListener("click", playpaper)
-document.getElementById("option3").addEventListener("click", playscissors)
-
-function playrock() {
+function play(userinput) {
   cs = choices[Math.floor(Math.random()*choices.length)];
-  document.getElementById("ps").innerHTML = "You Chose Rock";
+  document.getElementById("ps").innerHTML = "You Chose " + userinput;
   document.getElementById("cs").innerHTML = "The Computer Chose " + cs;
-  ps = "Rock"
+  ps = userinput;
   if (ps == cs) {
     document.getElementById("result").innerHTML = result[2];
   }
-  else if (cs == "Scissors") { 
-    document.getElementById("result").innerHTML = result[0]
+  else if (ps == "Rock" && cs == "Scissors") { 
+    document.getElementById("result").innerHTML = result[0];
+  }
+
+  else if (ps == "Scissors" && cs == "Paper") { 
+    document.getElementById("result").innerHTML = result[0];
+  }
+  else if (ps == "Paper" && cs == "Rock") { 
+    document.getElementById("result").innerHTML = result[0];
   }
   else { 
-    document.getElementById("result").innerHTML = result[1]
-  }
-}
-
-function playpaper() {
-  cs = choices[Math.floor(Math.random()*choices.length)];
-  document.getElementById("ps").innerHTML = "You Chose Paper";
-  document.getElementById("cs").innerHTML = "The Computer Chose " + cs;
-  ps = "Paper"
-  if (ps == cs) {
-    document.getElementById("result").innerHTML = result[2];
-  }
-  else if (cs == "Rock") { 
-    document.getElementById("result").innerHTML = result[0]
-  }
-  else { 
-    document.getElementById("result").innerHTML = result[1]
-  }
-}
-
-
-function playscissors() {
-  cs = choices[Math.floor(Math.random()*choices.length)];
-  document.getElementById("ps").innerHTML = "You Chose Scissors";
-  document.getElementById("cs").innerHTML = "The Computer Chose " + cs;
-  ps = "Scissors"
-  if (ps == cs) {
-    document.getElementById("result").innerHTML = result[2];
-  }
-  else if (cs == "Paper") { 
-    document.getElementById("result").innerHTML = result[0]
-  }
-  else { 
-    document.getElementById("result").innerHTML = result[1]
+    document.getElementById("result").innerHTML = result[1];
   }
 }
