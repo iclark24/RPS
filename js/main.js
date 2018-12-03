fd556a0d14d870db3a0be51b4549c824415b9115
ian <clarksware24@gmail.com>
2018-12-03 13:45:12 -0700

    add counter for wins draws losses

--- js/main.js
+++ js/main.js
@@ -1,36 +1,55 @@
-var result = ["You Win!", "You Lose!", "It's a Draw!"];
-var choices = ["Rock", "Paper", "Scissors"]
+const result = ["You Win!", "You Lose!", "It's a Draw!"];
+const choices = ["Rock", "Paper", "Scissors"]
 var win = 0
 var lose = 0
 var draw = 0
 
 function play(userinput) {
-  cs = choices[Math.floor(Math.random()*choices.length)];
+  var cs = choices[Math.floor(Math.random()*choices.length)];
   document.getElementById("ps").innerHTML = "You Chose " + userinput;
   document.getElementById("cs").innerHTML = "The Computer Chose " + cs;
-  ps = userinput;
+  var ps = userinput;
   if (ps == cs) {
     document.getElementById("result").innerHTML = result[2];
+    wld("Draw");
   }
   else if (ps == "Rock" && cs == "Scissors") { 
     document.getElementById("result").innerHTML = result[0];
+    wld("Win");
   }
 
   else if (ps == "Scissors" && cs == "Paper") { 
     document.getElementById("result").innerHTML = result[0];
+    wld("Win");
   }
   else if (ps == "Paper" && cs == "Rock") { 
     document.getElementById("result").innerHTML = result[0];
+    wld("Win");
   }
   else { 
     document.getElementById("result").innerHTML = result[1];
+    wld("Lose");
   }
 }
 
-function win(result){
+function wld(result){
   rs = result
-  if (result = "Draw"){
-    
+  if (result === "Draw"){
+    draw += 1
+    disply();
   }
+  else if (result === "Win"){
+    win += 1
+    disply();
+  }
+  else {
+    lose += 1
+    disply();
+  }
+}
 
+function disply(){
+    document.getElementById("wins").innerHTML = "You have won " + win + " times.";
+    document.getElementById("draws").innerHTML = "You have gotten a draw " + draw + " times.";
+    document.getElementById("losses").innerHTML = "You have lost " + lose + " times.";
 }
